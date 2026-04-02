77a7f7e8d280da170edb879071d24434abc9ab66
oTuin <serkansylr@gmail.com>
2026-04-02 19:14:29 +0300

    Revert "patch1.19"
    This reverts commit eb79d0372bc3cf27ea9060ec36091da0717ee2d7.

--- src/antifarm/AntiMobFarm.java
+++ src/antifarm/AntiMobFarm.java
@@ -30,20 +30,16 @@ public class AntiMobFarm implements Listener {
 
 	private final AntiFarmPlugin plugin;
 	private final Configuration config;
-    private static final Attribute maxmobHealth;
-    static {
-        Attribute maxhealth;
-        try {
-            maxhealth = (Attribute) Attribute.class.getField("MAX_HEALTH").get(null);
-        } catch (Exception e) {
-            try {
-                maxhealth = (Attribute) Attribute.class.getField("GENERIC_MAX_HEALTH").get(null);
-            } catch (Exception e2) {
-                maxhealth = null;
-            }
-        }
-        maxmobHealth = maxhealth;
-    }
+	private static final Attribute maxmobHealth;
+	static {
+		Attribute health;
+		try {
+			health = Attribute.valueOf("MAX_HEALTH");
+		} catch (IllegalArgumentException e) {
+			health = Attribute.valueOf("GENERIC_MAX_HEALTH");
+		}
+		maxmobHealth = health;
+	}
 
 	public AntiMobFarm(AntiFarmPlugin plugin) {
 		this.plugin = plugin;
