52a0484470b9c293ae6f3cb29a675e9f99a292ff
sillycorn <untesha04+sillycorn@gmail.com>
2026-04-06 04:17:15 +0300

    changed my mind be gone

--- src/main/java/core/AntiFarmPlugin.java
+++ src/main/java/core/AntiFarmPlugin.java
@@ -16,7 +16,6 @@ import antifarm.AntiEndermanFarm;
 import antifarm.AntiFishFarm;
 import antifarm.AntiFroglightFarm;
 import antifarm.AntiLavaFarm;
-//import antifarm.AntiLightlessFarm;
 import antifarm.AntiMobFarm;
 import antifarm.AntiMobSpawner;
 import antifarm.AntiPistonFarm;
@@ -55,7 +54,7 @@ public class AntiFarmPlugin extends JavaPlugin implements Listener {
 		antiCactusFarm = new AntiCactusFarm(this);
 		antiBarteringFarm = new AntiBarteringFarm(this);
 		registerEvents(this, new AntiPistonFarm(this), new AntiVillagerFarm(this), new AntiWaterFarm(this), antiCactusFarm,
-				new AntiEndermanFarm(this), new AntiVillagerBreed(this), new AntiMobFarm(this), /*new AntiLightlessFarm(this),*/
+				new AntiEndermanFarm(this), new AntiVillagerBreed(this), new AntiMobFarm(this),
 				antiDispenser, antiFishFarm, new AntiWaterlessFarm(this), new AntiMobSpawner(this),
 				new AntiVillagerTransform(this), new AntiVillagerTarget(this), new AntiVillageGuard(this), new AntiSnowballFarm(this),
 				new AntiRaidFarm(this), new AntiBerryFarm(this), new AntiZeroTickFarm(this),
